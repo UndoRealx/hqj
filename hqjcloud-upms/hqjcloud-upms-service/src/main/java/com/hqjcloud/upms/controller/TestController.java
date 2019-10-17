58bbe421f36b995328e2c837fc9193b0fcf7ea9d
alluserbegin <597153059@qq.com>
2019-10-17 10:16:00 +0800

    1

---
+++ hqjcloud-upms/hqjcloud-upms-service/src/main/java/com/hqjcloud/upms/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.hqjcloud.upms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @ProjectName: hqjcloud
+ * @Package: com.hqjcloud.upms.controller
+ * @ClassName: TestController
+ * @Description:
+ * @Author: lic
+ * @CreateDate: 2019/10/17 10:05
+ * @UpdateUser: 更新者
+ * @UpdateDate: 2019/10/17 10:05
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ */
+@Controller
+@RequestMapping("/test")
+public class TestController {
+}
